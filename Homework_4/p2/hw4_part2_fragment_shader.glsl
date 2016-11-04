ef241187bb6d28f0777a2ce9f96404785c012567
Adam Morris <adam@kernelpanicstudios.com>
2016-11-04 16:27:55 -0500

    I don't have to repeat the blend mode 0 code.

--- Homework_4/p2/hw4_part2_fragment_shader.glsl
+++ Homework_4/p2/hw4_part2_fragment_shader.glsl
@@ -16,12 +16,7 @@ void main(void)
 	vec4 landscape_texture_color = texture(landscape_texture, pass_TexCoord);
 	vec4 person_texture_color = texture(person_texture, pass_TexCoord);
 
-	if (texture_blend == 0) 
-	{
-		// This looks like a horrible Facebook profile picture.
-		color = (0.25 * pass_Color + landscape_texture_color + person_texture_color) * gradient_texture_color;
-	} 
-	else if (texture_blend == 1) 
+	if (texture_blend == 1) 
 	{
 		color = 0.1 * pass_Color * gradient_texture_color + landscape_texture_color + 0.5 * person_texture_color;
 	}
@@ -37,6 +32,7 @@ void main(void)
 	}
 	else 
 	{
+		// This looks like a horrible Facebook profile picture.
 		color = (0.25 * pass_Color + landscape_texture_color + person_texture_color) * gradient_texture_color;
 	}
 }
